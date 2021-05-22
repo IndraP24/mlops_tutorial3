fe8d207f7c5b62469a620f81b03ee879cdff0171
INDRASHIS PAUL <indrashis985@gmail.com>
2021-05-22 17:15:42 +0530

    Trying changed parameters on RF Classifier

--- train.py
+++ train.py
@@ -28,8 +28,8 @@ X = imp.transform(X)
 
 
 # Quadratic DA model
-clf = RandomForestClassifier()
-yhat = cross_val_predict(clf, X, y, cv=5)
+clf = RandomForestClassifier(max_depth=20)
+yhat = cross_val_predict(clf, X, y, cv=10)
 
 acc = np.mean(yhat==y)
 tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
